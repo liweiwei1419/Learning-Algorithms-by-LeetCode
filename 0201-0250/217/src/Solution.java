f8485225a1c9c1626ea926fb51a15423354f76fc
李威威 <121088825@qq.com>
2018-11-14 15:45:10 +0800

    添加了 217、219、220 判断是否有重复元素的系列问题解答。

---
+++ 0201-0250/217/src/Solution.java
@@ -0,0 +1,54 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 使用 hash 表进行检查
+ */
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return false;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.contains(num)) {
+                set.add(num);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsDuplicate1(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            } else {
+                set.add(num);
+            }
+        }
+        return false;
+    }
+
+    public boolean containsDuplicate2(int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return false;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            boolean success = set.add(num);
+            if (!success) {
+                // 如果没有添加成功，表示有重复元素，直接返回就可以了
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+}
