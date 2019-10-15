605bbd767c1deaf6e845e13c3d3d08010dbac2bc
李威 <121088825@qq.com>
2019-10-15 10:54:29 +0800

    添加了桶排序的问题。

---
+++ bucket-sort/0041-first-missing-positive/src/Solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+
+    public int firstMissingPositive(int[] nums) {
+        int len = nums.length;
+
+        for (int i = 0; i < len; i++) {
+            while (nums[i] > 0 && nums[i] <= len && nums[nums[i] - 1] != nums[i]) {
+                // 满足在指定范围内、并且没有放在正确的位置上，才交换
+                // 例如：数值 3 应该放在索引 2 的位置上
+                swap(nums, nums[i] - 1, i);
+            }
+        }
+
+        // [1, -1, 3, 4]
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        // 都正确则返回数组长度 + 1
+        return len + 1;
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        if (index1 == index2) {
+            return;
+        }
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 2, 1};
+        Solution solution = new Solution();
+        int firstMissingPositive = solution.firstMissingPositive(nums);
+        System.out.println(firstMissingPositive);
+    }
+}
---
+++ bucket-sort/0041-first-missing-positive/src/Solution2.java
@@ -0,0 +1,26 @@
+public class Solution2 {
+
+    public int firstMissingPositive(int[] nums) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            while (nums[i] > 0 && nums[i] <= len && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, nums[i] - 1, i);
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            if (nums[i] - 1 != i) {
+                return i + 1;
+            }
+        }
+        return len + 1;
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        if (index1 == index2) {
+            return;
+        }
+        nums[index1] = nums[index1] ^ nums[index2];
+        nums[index2] = nums[index1] ^ nums[index2];
+        nums[index1] = nums[index1] ^ nums[index2];
+    }
+}
