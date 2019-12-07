a1a66a47b0f24a159b8925e41ea388763d51952f
李威 <121088825@qq.com>
2019-12-07 13:26:30 +0800

    添加了滑动窗口经典问题。

--- sliding-window/0239-sliding-window-maximum/src/Solution.java
+++ sliding-window/0239-sliding-window-maximum/src/Solution.java
@@ -1,46 +1,43 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Deque;
+import java.util.LinkedList;
 
+/**
+ * @author liweiwei1419
+ * @date 2019/10/12 3:31 下午
+ */
 public class Solution {
 
+    /**
+     * 双端队列的做法
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
     public int[] maxSlidingWindow(int[] nums, int k) {
         int len = nums.length;
-        // 特判
         if (len == 0) {
-            return new int[]{};
+            return new int[0];
         }
-        // 结果集
-        List<Integer> res = new ArrayList<>();
-        // 滑动窗口，注意：保存的是索引值
-        ArrayDeque<Integer> deque = new ArrayDeque<>(k);
+        int[] res = new int[len - k + 1];
 
+        // 循环不变量：
+        // queue 中的元素，如果先进来的元素比后进来的元素小，先进来的元素需要从对尾弹出以后，后进来的元素才进来
+        // queue 首一定是当前滑动窗口内最大元素的索引
+
+        Deque<Integer> queue = new LinkedList<>();
         for (int i = 0; i < len; i++) {
-            // 当元素从左边界滑出的时候，如果它恰恰好是滑动窗口的最大值
-            // 那么将它弹出
-            if (i >= k && i - k == deque.getFirst()) {
-                deque.pollFirst();
+            if (i >= k && queue.getFirst() == i - k) {
+                queue.removeFirst();
             }
-
-            // 如果滑动窗口非空，新进来的数比队列里已经存在的数还要大
-            // 则说明已经存在数一定不会是滑动窗口的最大值（它们毫无出头之日）
-            // 将它们弹出
-            while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
-                deque.pollLast();
+            while (!queue.isEmpty() && nums[queue.getLast()] <= nums[i]) {
+                queue.removeLast();
             }
-            deque.add(i);
-            // 队首一定是滑动窗口的最大值的索引
+            queue.add(i);
             if (i >= k - 1) {
-                res.add(nums[deque.peekFirst()]);
+                res[i - k + 1] = nums[queue.getFirst()];
             }
         }
-
-        int size = res.size();
-        int[] result = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            result[i] = res.get(i);
-        }
-        return result;
+        return res;
     }
 }
