1e7f368086df4a388923a11423f025b0331a2a71
李威 <121088825@qq.com>
2019-12-07 13:28:17 +0800

    修正了在旋转数组中查找元素。

--- binary-search/0033-search-in-rotated-sorted-array/src/Solution3.java
+++ binary-search/0033-search-in-rotated-sorted-array/src/Solution3.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution3 {
 
     // 中间元素和右边界比较
 
--- binary-search/0033-search-in-rotated-sorted-array/src/Solution4.java
+++ binary-search/0033-search-in-rotated-sorted-array/src/Solution4.java
@@ -3,4 +3,14 @@
  * @date 2019/11/28 9:47 上午
  */
 public class Solution4 {
+
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
